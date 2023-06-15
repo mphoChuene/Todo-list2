c9d1ba0d6f7cca0bf7e7eee032fd070d1edd4caa
mphoChuene <mphochuene42@gmail.com>
2023-06-15 11:59:56 +0200

    uncomplete UI

--- src/Todo-list.jsx
+++ src/Todo-list.jsx
@@ -1,22 +1,21 @@
 import React, { useState } from "react";
 import "./App.css";
-/* import React, { useState } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Login from "./Login";
-import Signup from "./Signup"; */
 
 function TodoList() {
   const [todos, setTodos] = useState([]);
   const [inputValue, setInputValue] = useState("");
+  const [priorityValue, setPriorityValue] = useState("normal");
 
   const addTodo = () => {
     if (inputValue !== "") {
       const newTodo = {
         id: Math.random(),
         text: inputValue,
+        priority: priorityValue,
       };
       setTodos([...todos, newTodo]);
       setInputValue("");
+      setPriorityValue("normal");
     }
   };
 
@@ -25,10 +24,10 @@ function TodoList() {
     setTodos(updatedTodos);
   };
 
-  const updateTodo = (id, newText) => {
+  const updateTodo = (id, newText, newPriority) => {
     const updatedTodos = todos.map((todo) => {
       if (todo.id === id) {
-        return { ...todo, text: newText };
+        return { ...todo, text: newText, priority: newPriority };
       }
       return todo;
     });
@@ -37,32 +36,45 @@ function TodoList() {
 
   return (
     <>
-      <div className="container">
-        <h1>Todo List</h1> <br />
-        <div>
-          <input
-            type="text"
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
-          />
-          <button onClick={addTodo}>Add</button>
-          <ul>
-            {todos.map((todo) => (
-              <li key={todo.id}>
-                <input
-                  type="text"
-                  value={todo.text}
-                  onChange={(e) => updateTodo(todo.id, e.target.value)}
-                />
-                <button onClick={() => deleteTodo(todo.id)}>Delete</button>
-              </li>
-            ))}
-          </ul>
+      <div className="main">
+        <div className="container">
+          <h1>Todo List</h1>
+          <br />
+          <div>
+            <input
+              type="text"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+            />
+            <select
+              value={priorityValue}
+              onChange={(e) => setPriorityValue(e.target.value)}
+            >
+              <option value="normal">Normal</option>
+              <option value="medium">Medium</option>
+              <option value="important">Important</option>
+            </select>
+            <button onClick={addTodo}>Add</button>
+            <ul>
+              {todos.map((todo) => (
+                <li key={todo.id} className={`priority-${todo.priority}`}>
+                  <input
+                    type="text"
+                    value={todo.text}
+                    onChange={(e) =>
+                      updateTodo(todo.id, e.target.value, priorityValue)
+                    }
+                  />
+                  <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
 
-      <div class="area">
-        <ul class="circles">
+      <div className="area">
+        <ul className="circles">
           <li></li>
           <li></li>
           <li></li>
